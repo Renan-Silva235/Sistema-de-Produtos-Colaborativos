53694a850d32faee42aabe824dd62874d957e22e
Renan-Silva235 <renan.rubio95@gmail.com>
2025-09-08 15:52:52 -0300

    update

--- crud_C/crud.c
+++ crud_C/crud.c
@@ -2,7 +2,22 @@
 #include <stdlib.h>
 #include <cjson/cJSON.h>
 #include <errno.h>
-#include "../include/gerarId.h"
+
+
+int gerarId(cJSON *json) {
+    int id = 0;
+    int tamanhoArquivoJson = cJSON_GetArraySize(json);
+    for (int i = 0; i < tamanhoArquivoJson; i++) {
+        cJSON *item = cJSON_GetArrayItem(json, i);
+        cJSON *id_json = cJSON_GetObjectItem(item, "id");
+        if (cJSON_IsNumber(id_json) && id_json->valueint > id) {
+            id = id_json->valueint;
+        }
+    }
+    return id + 1;
+}
+
+
 
 
 
