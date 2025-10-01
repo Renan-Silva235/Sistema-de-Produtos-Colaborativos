4892bdaf5692a2397f858285027aa96f881763ae
Renan-Silva235 <renan.rubio95@gmail.com>
2025-10-01 11:02:04 -0300

    .

--- telas/solicitacoes/tela_pedidos.py
+++ telas/solicitacoes/tela_pedidos.py
@@ -1,50 +1,108 @@
+import json
 from utils.sistema.sistema import limpar_tela
 from usuarios.gerenciador import Gerenciador
 from utils.exibir_tabela.exibir import exibir_tabela
 class TelaPedidos:
-    def __init__(self):
+    def __init__(self, usuario):
+        """
+        Inicializa a tela de pedidos solicitados.
+
+        Recebe o usuário e seus respectivos arquivos json que serão manipulados.
+
+        self.usuario: o usuário que está acessando a tela
+        self.json_estoque: o caminho do arquivo json que contém os produtos em estoque
+        self.json_aprovados: o caminho do arquivo json que contém os produtos aprovados
+        self.json_pedidos: o caminho do arquivo json que contém os produtos pedidos
+        self.json_reprovados: o caminho do arquivo json que contém os produtos reprovados
+        self.pedidos: o gerenciador do arquivo json que contém os produtos pedidos
+        self.aprovados: o gerenciador do arquivo json que contém os produtos aprovados
+        self.reprovado: o gerenciador do arquivo json que contém os produtos reprovados
+        self.estoque: o gerenciador do arquivo json que contém os produtos em estoque
+        self.iniciar: um booleano que indica se a tela deve ser exibida novamente
+        self.status: uma lista que contém os status possíveis para um produto (aprovado ou reprovado)
+        """
+        self.usuario = usuario
+        self.json_estoque = "jsons/produtos/produtos.json"
+        self.json_aprovados = "jsons/solicitacoes/aprovados.json"
         self.json_pedidos = "jsons/solicitacoes/pedidos.json"
+        self.json_reprovados = "jsons/solicitacoes/reprovados.json"
         self.pedidos = Gerenciador(self.json_pedidos)
+        self.aprovados = Gerenciador(self.json_aprovados)
+        self.reprovado = Gerenciador(self.json_reprovados)
+        self.estoque = Gerenciador(self.json_estoque)
+        self.iniciar = True
+        self.status = ["Aprovado", "Reprovado"]
 
     def mostrar(self):
-        print("PEDIDOS SOLICITADOS: \n\n")
+        """
+        Mostra a tela de pedidos solicitados no terminal.
 
-        categorias = ["Vestuário", "Medicamentos", "Alimentícios"]
+        Exibe todos os produtos solicitados e permite ao usuário aprovar ou reprovar o pedido.
+        Se o usuário aprovar o pedido, o produto é adicionado à lista de aprovados e sua quantidade é atualizada no estoque.
+        Se o usuário reprovar o pedido, o produto é removido da lista de pedidos.
+        """
+        while self.iniciar:
+            print("PEDIDOS SOLICITADOS: \n\n")
 
-        for pedido in self.pedidos.listar():
-            if pedido["categoria"] == categorias[0]:
-                produto = {
-                    "identificador": pedido["id"],
-                    "categoria": pedido["categoria"],
-                    "nome_produto": pedido["nome_produto"],
-                    "marca": pedido["marca"],
-                    "cor": pedido["cor"],
-                    "tamanho": pedido["tamanho"]
-                }
-                exibir_tabela(produto)
-            elif pedido["categoria"] == categorias[1]:
-                produto = {
-                    "identificador": pedido["id"],
-                    "categoria": pedido["categoria"],
-                    "nome_medicamento": pedido["nome_medicamento"],
-                    "apresentacao": pedido["apresentacao"],
-                    "dosagem": pedido["dosagem"],
-                    "validade": pedido["validade"]
-                }
+            for pedido in self.pedidos.listar():
+                for produto in pedido["pedido"]:
+                    exibir_tabela(produto)
 
-                exibir_tabela(produto)
-            elif pedido["categoria"] == categorias[2]:
-                produto = {
-                    "identificador": pedido["id"],
-                    "categoria": pedido["categoria"],
-                    "nome_alimento": pedido["nome_alimento"],
-                    "peso": pedido["peso"],
-                    "validade": pedido["validade"]
-                }
+            pegar_id = int(input("\nDigite o número do Id correspondente ou '0' para voltar: "))
 
-                exibir_tabela(produto)
+            if pegar_id == 0:
+                limpar_tela()
+                return
+
+            for pedido in self.pedidos.listar():
+                for produto in pedido["pedido"]:
+                    if pegar_id == produto["id"]:
+                        status = int(input("""
+                                           Digite a opção com a ação desejada:
+                                           1- Aprovado
+                                           2- reprovado
+                                           0- voltar
+
+                                           opção: """))
+
+                        if status == 1:
+
+                            produto["id_responsavel"] = self.usuario["id"]
+                            produto["status"] = self.status[0]
+                            del pedido["id"]
+                            self.aprovados.cadastrar(pedido)
+
+                            for itens in self.estoque.listar():
+                                if pegar_id == itens["id"]:
+                                    estoque_atualizado = itens["quantidade"] - produto["quantidade_pedida"]
+                                    # alterar = Alteracoes()
+                                    self.estoque.atualizar("id", itens["quantidade"], estoque_atualizado)
+                                    # alterar.alterar_estoque(self.json_estoque, pegar_id, estoque_atualizado)
+
+                            pedidos = [p for p in self.pedidos.listar() if p.get("id") != pegar_id]
+
+                            with open(self.json_pedidos, "w", encoding="utf-8") as f:
+                                json.dump(pedidos, f, indent=4, ensure_ascii=False)
+
+                            break
+
+                        elif status == 2:
+                            produto["id_responsavel"] = self.usuario["id"]
+                            produto["status"] = self.status[1]
+                            self.reprovado.cadastrar(pedido)
+
+                            pedidos = [p for p in self.pedidos.listar() if p.get("id") != pegar_id]
+
+                            with open(self.json_reprovados, "w", encoding="utf-8") as f:
+                                json.dump(pedidos, f, indent=4, ensure_ascii=False)
+
+                            continue
+                        elif status == 0:
+                            limpar_tela()
+                            break
 
 
 
-tela = TelaPedidos()
-tela.mostrar()
+
+# tela = TelaPedidos("Renan")
+# tela.mostrar()
--- telas/solicitantes/tela_solicitantes.py
+++ telas/solicitantes/tela_solicitantes.py
@@ -8,48 +8,24 @@ class TelaSolicitantes:
         self.usuario = usuario
         self.json_pedidos = "jsons/solicitacoes/pedidos.json"
         self.json_produtos = "jsons/produtos/produtos.json"
+        self.json_aprovados = "jsons/solicitacoes/aprovados.json"
+        self.json_reprovados = "jsons/solicitacoes/reprovados.json"
         self.produtos = Gerenciador(self.json_produtos)
         self.pedidos = Gerenciador(self.json_pedidos)
+        self.produtos_aprovados = Gerenciador(self.json_aprovados)
+        self.produtos_reprovados = Gerenciador(self.json_reprovados)
         self.iniciar = True
         self.categorias = ["Vestuário", "Medicamentos", "Alimentícios"]
-
-        self.vestuario = {
-                            "identificador": None,
-                            "categoria": None,
-                            "nome_produto": None,
-                            "marca": None,
-                            "cor": None,
-                            "tamanho": None,
-                            "quantidade_disponivel": None,
-                        }
-
-        self.medicamentos = {
-                            "identificador": None,
-                            "categoria": None,
-                            "nome_medicamento": None,
-                            "dosagem": None,
-                            "validade": None,
-                            "quantidade_disponivel": None,
-                            }
-
-        self.alimentos = {
-                            "identificador": None,
-                            "categoria": None,
-                            "nome_alimento": None,
-                            "peso": None,
-                            "validade": None,
-                            "quantidade_disponivel": None,
-                        }
-
 
     def mostrar(self):
 
         print(f"Solicitante: {self.usuario["nome"]}\n\n")
         while self.iniciar:
             print("""
-                  Seja Bem-Vindo(a) ao nosso sistema de Doações, aqui você poderá consultar alguns produtos que estão disponíveis
-                  para doação e fazer pedidos. Seus pedidos serão avaliados pela nossa equipe de Administração, caso seja aprovado ou não
-                  você será notificado(a) pela aba pedidos. Em caso de aprovação, seu pedido será entregue no seu endereço cadastrado.\n\n""")
+                Seja Bem-Vindo(a) ao nosso sistema de Doações, aqui você poderá consultar alguns produtos que estão disponíveis
+                para doação e fazer pedidos. Seus pedidos serão avaliados pela nossa equipe de Administração, caso seja aprovado
+                ou não você será notificado(a) pela aba pedidos. Em caso de aprovação, seu pedido será entregue no seu endereço
+                cadastrado.\n\n""")
 
             print("1 - Consultar Produto")
             print("2 - Fazer pedido")
@@ -61,140 +37,18 @@ class TelaSolicitantes:
 
                 if opcao == 1:
                     limpar_tela()
-                    digitar_produto = input("Digite o nome do produto desejável: ").title()
-
-                    consultar_produto = self.produtos.consulta(digitar_produto)
-
-                    if not consultar_produto:
-                        print("Nenhum Produto encontrado\n\n")
-                        continue
-
-                    for item in consultar_produto:
-                        if item["categoria"] == self.categorias[0]:
-                            self.vestuario["identificador"] = item["id"]
-                            self.vestuario["categoria"] = item["categoria"]
-                            self.vestuario["nome_produto"] = item["nome_produto"]
-                            self.vestuario["marca"] = item["marca"]
-                            self.vestuario["cor"] = item["cor"]
-                            self.vestuario["tamanho"] = item["tamanho"]
-                            self.vestuario["quantidade_disponivel"] = item["quantidade"]
-                            exibir_tabela(self.vestuario)
-
-                        elif item["categoria"] == self.categorias[1]:
-                            self.medicamentos["identificador"] = item["id"]
-                            self.medicamentos["categoria"] = item["categoria"]
-                            self.medicamentos["nome_medicamento"] = item["nome_medicamento"]
-                            self.medicamentos["dosagem"] = item["dosagem"]
-                            self.medicamentos["validade"] = item["validade"]
-                            self.medicamentos["quantidade_disponivel"] = item["quantidade_disponivel"]
-                            exibir_tabela(self.medicamentos)
-
-                        elif item["categoria"] == self.categorias[2]:
-                            self.alimentos["identificador"] = item["id"]
-                            self.alimentos["categoria"] = item["categoria"]
-                            self.alimentos["nome_alimento"] = item["nome_alimento"]
-                            self.alimentos["peso"] = item["peso"]
-                            self.alimentos["validade"] = item["validade"]
-                            self.alimentos["quantidade_disponivel"] = item["quantidade"]
-                            exibir_tabela(self.alimentos)
-
+                    self._consultar_produtos()
+                    continue
 
                 elif opcao == 2:
                     limpar_tela()
-                    while True:
-                        print("PRODUTOS DISPONÍVEIS: \n")
-
-                        for produto in self.produtos.listar():
-                            if produto["categoria"] == self.categorias[0]:
-                                self.vestuario["identificador"] = produto["id"]
-                                self.vestuario["categoria"] = produto["categoria"]
-                                self.vestuario["nome_produto"] = produto["nome_produto"]
-                                self.vestuario["marca"] = produto["marca"]
-                                self.vestuario["cor"] = produto["cor"]
-                                self.vestuario["tamanho"] = produto["tamanho"]
-                                self.vestuario["quantidade_disponivel"] = produto["quantidade"]
-                                exibir_tabela(self.vestuario)
-
-                            elif produto["categoria"] == self.categorias[1]:
-                                self.medicamentos["identificador"] = produto["id"]
-                                self.medicamentos["categoria"] = produto["categoria"]
-                                self.medicamentos["nome_medicamento"] = produto["nome_medicamento"]
-                                self.medicamentos["dosagem"] = produto["dosagem"]
-                                self.medicamentos["validade"] = produto["validade"]
-                                self.medicamentos["quantidade_disponivel"] = produto["quantidade"]
-                                exibir_tabela(self.medicamentos)
-
-                            elif produto["categoria"] == self.categorias[2]:
-                                self.alimentos["identificador"] = produto["id"]
-                                self.alimentos["categoria"] = produto["categoria"]
-                                self.alimentos["nome_alimento"] = produto["nome_alimento"]
-                                self.alimentos["peso"] = produto["peso"]
-                                self.alimentos["validade"] = produto["validade"]
-                                self.alimentos["quantidade_disponivel"] = produto["quantidade"]
-                                exibir_tabela(self.alimentos)
-
-
-                        try:
-                            print("\n")
-                            pegar_identificador = int(input("Digite o número do identificador do produto desejado ou '0' para voltar: "))
-
-                            if pegar_identificador == 0:
-                                limpar_tela()
-                                break
-
-                            for itens in self.produtos.listar():
-
-                                if pegar_identificador == itens["id"]:
-                                    pegar_quantidade = int(input("digite a quantidade desejada: "))
-
-                                    if pegar_quantidade > itens["quantidade"]:
-                                        limpar_tela()
-                                        print("Quantidade excedeu a quantidade total de produtos.")
-                                        time.sleep(1.5)
-                                        limpar_tela()
-                                        continue
-
-                                    del itens["quantidade"]
-                                    del itens["id_doador"]
-                                    del itens["id_responsavel"]
-                                    itens["quantidade_pedida"] = pegar_quantidade
-                                    itens["id_solicitante"] = self.usuario["id"]
-                                    itens["status"] = "Em Análise"
-
-                                    chave_pedido = f"pedido"
-                                    pedido = {chave_pedido: []}
-
-                                    pedido[chave_pedido].append(itens)
-
-
-                                    self.pedidos.cadastrar(pedido)
-                                    limpar_tela()
-                                    print("Pedido enviado")
-                                    time.sleep(1.5)
-                                    limpar_tela()
-                                    break
-
-                                # else:
-                                    # print("Identificador não encontrado")
-
-
-                        except ValueError:
-                            limpar_tela()
-                            continue
+                    self._fazer_pedido()
+                    continue
 
                 elif opcao == 3:
                     limpar_tela()
-                    print("Solicitações enviadas: \n\n")
-
-                    meus_pedidos = self.pedidos.listar()
-
-                    for pedido in meus_pedidos:
-                        for produto in pedido["pedido"]:
-                            if produto["id_solicitante"] == self.usuario["id"]:
-                                exibir_tabela(produto)
-
-                    input("\nTecle enter para voltar")
-                    limpar_tela()
+                    self._status_pedidos()
+                    continue
 
                 elif opcao == 0:
                     if opcao == 0:
@@ -209,6 +63,146 @@ class TelaSolicitantes:
             except ValueError:
                 limpar_tela()
                 continue
+
+    def _consultar_produtos(self):
+        """
+        Mostra a tela de consultar produtos no terminal.
+
+        Exibe todos os produtos disponíveis e permite ao usuário digitar o nome do produto desejável.
+        Se o produto for encontrado, ele exibe as informações do produto e as quantidades disponíveis.
+        Se o produto não for encontrado, ele exibe uma mensagem de erro e volta para a tela inicial.
+        """
+
+        digitar_produto = input("Digite o nome do produto desejável: ").title()
+
+        consultar_produto = self.produtos.consulta(digitar_produto)
+
+        if not consultar_produto:
+            print("Nenhum Produto encontrado\n\n")
+            return
+
+        for item in consultar_produto:
+            item["quantidade_disponivel"] = item["quantidade"]
+            del item["quantidade"]
+            del item["id_doador"]
+            del item["id_responsavel"]
+            exibir_tabela(item)
+
+
+        print("\n\n")
+        input("Pressione enter para voltar")
+        limpar_tela()
+        return
+
+    def _fazer_pedido(self):
+
+        """
+        Mostra a tela de fazer pedido no terminal.
+
+        Exibe todos os produtos disponíveis e permite ao usuário digitar o identificador do produto desejado.
+        Se o produto for encontrado, ele exibe as informações do produto e as quantidades disponíveis.
+        Se o produto não for encontrado, ele exibe uma mensagem de erro e volta para a tela inicial.
+        """
+        while True:
+            print("PRODUTOS DISPONÍVEIS: \n")
+
+            for produto in self.produtos.listar():
+                produto["quantidade_disponivel"] = produto["quantidade"]
+                del produto["quantidade"]
+                del produto["id_doador"]
+                del produto["id_responsavel"]
+                exibir_tabela(produto)
+
+
+            try:
+                print("\n")
+                pegar_identificador = int(input("Digite o número do identificador do produto desejado ou '0' para voltar: "))
+
+                if pegar_identificador == 0:
+                    limpar_tela()
+                    return
+
+
+
+                for itens in self.produtos.listar():
+                    if pegar_identificador in self.pedidos.listar():
+                        print("Pedido já enviado")
+                        time.sleep(1.5)
+                        limpar_tela()
+                        continue
+
+                    if pegar_identificador == itens["id"]:
+                        pegar_quantidade = int(input("digite a quantidade desejada: "))
+
+                        if pegar_quantidade > itens["quantidade"]:
+                            limpar_tela()
+                            print("Quantidade excedeu a quantidade total de produtos.")
+                            time.sleep(1.5)
+                            limpar_tela()
+                            continue
+
+
+
+                        del itens["quantidade"]
+                        del itens["id_doador"]
+                        del itens["id_responsavel"]
+                        itens["quantidade_pedida"] = pegar_quantidade
+                        itens["id_solicitante"] = self.usuario["id"]
+                        itens["status"] = "Em Análise"
+
+                        chave_pedido = f"pedido"
+                        pedido = {chave_pedido: []}
+
+                        pedido[chave_pedido].append(itens)
+
+
+                        self.pedidos.cadastrar(pedido)
+                        limpar_tela()
+                        print("Pedido enviado")
+                        time.sleep(1.5)
+                        limpar_tela()
+                        break
+
+                    # else:
+                        # print("Identificador não encontrado")
+
+
+            except ValueError:
+                limpar_tela()
+                continue
+
+    def _status_pedidos(self):
+        """
+        Mostra a tela de status de pedidos solicitados no terminal.
+
+        Aqui o usuário apenas visualiza se o pedido dele está ainda em análise ou se foi aprovado ou reprovado.
+        """
+        print("Solicitações enviadas: \n\n")
+
+        meus_pedidos = self.pedidos.listar()
+
+        for pedido in meus_pedidos:
+            for produto in pedido["pedido"]:
+                if produto["id_solicitante"] == self.usuario["id"]:
+                    exibir_tabela(produto)
+
+        for aprovado in self.produtos_aprovados.listar():
+            for produto in aprovado["pedido"]:
+                if produto["id_solicitante"] == self.usuario["id"]:
+                    exibir_tabela(produto)
+
+        for reprovado in self.produtos_reprovados.listar():
+            for produto in reprovado["pedido"]:
+                if produto["id_solicitante"] == self.usuario["id"]:
+                    exibir_tabela(produto)
+
+
+        input("\nTecle enter para voltar")
+        limpar_tela()
+        return
+
+
+
 
 
 
